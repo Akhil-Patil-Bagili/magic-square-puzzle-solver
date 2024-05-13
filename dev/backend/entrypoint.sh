2e45408f625f689413dcbb1d46b9e39ffc7766d2
Ankith-Harsha-Akhil <akhilpatil.bagili@sjsu.edu>
2024-05-12 23:37:50 -0700

    added week14 report

--- dev/backend/entrypoint.sh
+++ dev/backend/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# Run database migrations
+echo "Running database migrations"
 flask db upgrade
-exec "$@"
+
+# Start the application
+if [ "$FLASK_ENV" = "development" ]; then
+  exec flask run --host=0.0.0.0
+else
+  exec gunicorn --bind 0.0.0.0:5000 wsgi:app
+fi
